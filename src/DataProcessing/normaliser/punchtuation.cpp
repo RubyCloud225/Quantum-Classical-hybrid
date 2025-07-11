a1daa37456c43599c415712b24744befde1f5a5e
Catherine Earl <116440382+RubyCloud225@users.noreply.github.com>
2025-07-11 17:58:43 +0100

    removal of redundent code

--- src/DataProcessing/normaliser/punchtuation.cpp
+++
@@ -1,4 +0,0 @@
-// bring in punctuation characters from the C++ standard library
-// spilt per behavour
-// split per behaviour isolated
-// split per behaviour isolated and remove
--- src/DataProcessing/normaliser/sequence.cpp
+++
@@ -1,3 +0,0 @@
-// create vectors for sequence processing
-// generate a sequence from a given string
-// get the data from the points // get the data from the points with a given offset
