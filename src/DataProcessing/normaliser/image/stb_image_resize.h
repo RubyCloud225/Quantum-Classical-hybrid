b6ac35f229052fa751b46e7a48c756182c8e7ccb
Catherine Earl <116440382+RubyCloud225@users.noreply.github.com>
2025-08-24 20:57:23 +0100

    deleting stb

--- src/DataProcessing/normaliser/image/stb_image_resize.h
+++
@@ -1,9 +0,0 @@
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "stb_image_resize.h"
-
-std::vector<unsigned char> out(224*224*3); // target size
-stbir_resize_uint8(
-    pixels, w, h, 0, // src buffer + src stride
-    out.data(), 224, 224, 0, // dst buffer + dst stride
-    3 // channels
-);
